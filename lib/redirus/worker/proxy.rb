8322b5d743cb9c94419d678949fc9fff68a51f17
Marek Kasztelnik <mkasztelnik@gmail.com>
2013-08-06 14:01:48 +0200

    proxy worker implementation

--- lib/redirus/worker/proxy.rb
+++ lib/redirus/worker/proxy.rb
@@ -6,7 +6,57 @@ module Redirus
       include Sidekiq::Worker
 
       def perform(proxies=[], properties=[])
-        puts "Generating proxies configuration #{proxies} #{properties}"
+        proxy_config = proxy_config(proxies, properties)
+
+        begin
+          update_http  proxy_config
+          update_https proxy_config
+          restart_nginx
+        rescue Errno::EACCES
+          $stderr << "Error: Cannot write to config files\n"
+          raise
+        rescue Errno::ESRCH
+          $stderr << "Warning: Nginx is dead - continuing\n"
+        end
+      end
+
+      def self.generator
+        @@generator || Redirus::Worker::NginxConfigGenerator
+      end
+
+      # XXX: is it possible to use something like cattr_writer?
+      def self.generator=(generator)
+        @@generator = generator
+      end
+
+      private
+
+      def config
+        @config = Redirus::Worker.config
+      end
+
+      def proxy_config(proxies, properties)
+        self.class.generator.new(proxies, properties).generate
+      end
+
+      def update_http(http_proxy_config)
+        write_proxy_config config.http_proxy_file, config.http_upstream_file, http_proxy_config[:http]
+      end
+
+      def update_https(https_proxy_config)
+        write_proxy_config config.https_proxy_file, config.https_upstream_file, https_proxy_config[:https]
+      end
+
+      def restart_nginx
+        File.open(config.nginx_pid_file) do |file|
+          pid = file.read.to_i
+          Process.kill :SIGHUP, pid
+        end
+      end
+
+      def write_proxy_config(proxy_file, upstream_file, config)
+        File.open(proxy_file, 'w') { |file| file.write config[:proxy] }
+        File.open(upstream_file, 'w') { |file| file.write config[:upstream] }
       end
     end
   end
--- spec/redirus/worker/proxy_spec.rb
+++ spec/redirus/worker/proxy_spec.rb
@@ -1,11 +1,106 @@
 require 'spec_helper'
 
-describe Redirus::Worker::Proxy, 'nginx configuration generation' do
+describe Redirus::Worker::Proxy do
   let(:worker) { Redirus::Worker::Proxy.new }
 
-  context 'when proxies list is empty' do
-    it 'generates empty configurations when no redirections' do
+  describe '.generator' do
+    context "when overridden" do
+      let(:generator) { double("mocked generator") }
+      before          { Redirus::Worker::Proxy.generator = generator }
+      it              { expect(Redirus::Worker::Proxy.generator).to eq(generator) }
+    end
 
+    context "when not set" do
+      before { Redirus::Worker::Proxy.generator = nil }
+      it     { expect(Redirus::Worker::Proxy.generator).to eq(Redirus::Worker::NginxConfigGenerator) }
+    end
+  end
+
+  context 'nginx configuration generator' do
+    let(:config) {
+      double('worker configuration',
+        http_proxy_file: 'http_proxy_file',
+        http_upstream_file: 'http_upstream_file',
+        https_proxy_file: 'https_proxy_file',
+        https_upstream_file: 'https_upstream_file',
+        nginx_pid_file: 'nginx_pid_file'
+      )
+    }
+    let(:generator) {
+      double('nginx config generator', generate: {
+        http:  { proxy: 'http proxy',  upstream: 'http upstream' },
+        https: { proxy: 'https proxy', upstream: 'https upstream' }
+      })
+    }
+    let(:generator_class) { double('nginx config generator class', new: generator) }
+    let(:http_proxy_file) { double('http proxy file') }
+    let(:http_upstream_file) { double('http upstream file') }
+    let(:https_proxy_file) { double('https proxy file') }
+    let(:https_upstream_file) { double('https upstream file') }
+    let(:nginx_pid_file) { double('https upstream file') }
+
+    let(:worker) { Redirus::Worker::Proxy.new }
+
+    let!(:process) { Process.stub(:kill) }
+
+    before {
+      File.stub(:open) do |file_name, mode|
+        case file_name
+          when 'http_proxy_file'     then http_proxy_file
+          when 'http_upstream_file'  then http_upstream_file
+          when 'https_proxy_file'    then https_proxy_file
+          when 'https_upstream_file' then https_upstream_file
+          when 'nginx_pid_file'      then nginx_pid_file
+        end
+      end
+
+      Redirus::Worker.stub(:config).and_return(config)
+      Redirus::Worker::Proxy.generator = generator_class
+    }
+
+    after { Redirus::Worker::Proxy.generator = nil }
+
+    it 'uses generator to create configuration' do
+      expect(generator_class).to receive(:new).with('proxies', 'properties')
+      expect(generator).to receive(:generate)
+      worker.perform('proxies', 'properties')
+    end
+
+    it 'generates http proxy configuration' do
+      allow(File).to receive(:open).with('http_proxy_file', 'w').and_yield(http_proxy_file)
+      expect(http_proxy_file).to receive(:write).with(generator.generate[:http][:proxy])
+
+      worker.perform('proxies', 'properties')
+    end
+
+    it 'generates http upstream configuration' do
+      allow(File).to receive(:open).with('http_upstream_file', 'w').and_yield(http_upstream_file)
+      expect(http_upstream_file).to receive(:write).with(generator.generate[:http][:upstream])
+
+      worker.perform('proxies', 'properties')
+    end
+
+    it 'generates https proxy configuration' do
+      allow(File).to receive(:open).with('https_proxy_file', 'w').and_yield(https_proxy_file)
+      expect(https_proxy_file).to receive(:write).with(generator.generate[:https][:proxy])
+
+      worker.perform('proxies', 'properties')
+    end
+
+    it 'generates https upstream configuration' do
+      allow(File).to receive(:open).with('https_upstream_file', 'w').and_yield(https_upstream_file)
+      expect(https_upstream_file).to receive(:write).with(generator.generate[:https][:upstream])
+
+      worker.perform('proxies', 'properties')
+    end
+
+    it 'restarts nginx' do
+      allow(nginx_pid_file).to receive(:read).and_return('123')
+      allow(File).to receive(:open).with('nginx_pid_file').and_yield(nginx_pid_file)
+
+      expect(Process).to receive(:kill).with(:SIGHUP, 123)
+
+      worker.perform('proxies', 'properties')
     end
   end
 end
